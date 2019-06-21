1938bbc14ff7fb158832c675372eea7bf1feb3dd
Антонов Антон <antonov.a.v@outlook.com>
2019-06-21 20:12:56 +0300

    Add files via upload

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,24 +10,40 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите значение первого числа...");
-            string s = Console.ReadLine();
-            Console.WriteLine("Введите значение второго числа...");
-            string s1 = Console.ReadLine();
-            Console.WriteLine("Введите значение третьего числа...");
-            string s2 = Console.ReadLine();
+            
 
-            int a = Convert.ToInt32(s);
-            int a1 = Convert.ToInt32(s1);
-            int a2 = Convert.ToInt32(s2);
+            try
+            {
+                Console.WriteLine("Введите значение первого числа...");
+                string s = Console.ReadLine();
+                Console.WriteLine("Введите значение второго числа...");
+                string s1 = Console.ReadLine();
+                Console.WriteLine("Введите значение третьего числа...");
+                string s2 = Console.ReadLine();
 
-            Console.WriteLine("Выполняется операция на числами...");
 
-            int Summ = a * a1 ^ a2;
 
-            Console.WriteLine("езультат операции на числами..." + Summ);
+                Console.WriteLine("Выполняется операция на числами...");
+                int a = Convert.ToInt32(s);
+                int a1 = Convert.ToInt32(s1);
+                int a2 = Convert.ToInt32(s2);
+                int Summ = a * a1 ^ a2;
 
-            Console.ReadKey();
+                //Console.ReadKey();
+
+                Console.WriteLine("езультат операции на числами..." + Summ);
+                Console.ReadKey();
+            }
+            catch (ArithmeticException AE)
+            {
+
+                Console.WriteLine(AE.Message);
+                Console.ReadKey();
+                // throw;
+            }
+
+
+           
 
 
 
